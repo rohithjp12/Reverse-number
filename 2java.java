08ca7cd38c4a413290f833062e7af93160a2c49d
Rohit kumar <96667345+rohithjp12@users.noreply.github.com>
2022-01-23 02:14:24 +0530

    Add files via upload

---
+++ 2java.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+class myname
+{ 
+public static void main(String args[])
+{
+	 Scanner s1=new Scanner (System.in);
+	System.out.println("enter your number");
+	 int a=s1.nextInt();
+System.out.println("value of this number:-"+a);
+}
+}
